db913b4f8a48f95828abe3d832d109c629b071cb
BangHuynh0404 <huynhbangcong@gmail.com>
2022-01-16 17:14:20 +0700

    setheader

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
   res.write('<h1> Hello from my Node.js server!</h1>');
   res.end();
 });
