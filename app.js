f887abc54f3e07ae4959cfac0b3bd7cee047d0b8
BangHuynh0404 <huynhbangcong@gmail.com>
2022-01-17 22:12:50 +0700

    lab1.6

---
+++ app.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const fs = require('fs');
+const { PassThrough } = require('stream');
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title> Enter message </title></html>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log('day la chunk: ', chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      console.log(message);
+      fs.writeFileSync('message.txt', message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first page</title></html>');
+  res.write('<h1> Hello from my Node.js server!</h1>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(3000);
