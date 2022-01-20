0a7e36f4ce49941f2832fb9a06ca0101a8551ddc
BangHuynh0404 <huynhbangcong@gmail.com>
2022-01-21 01:31:41 +0700

    lab3.1

---
+++ app.js
@@ -0,0 +1,22 @@
+//NODE Dependencies
+const http = require('http');
+const path = require('path');
+//3rd Dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+//Main
+const app = express();
+
+const adminData = require('./routes/admin');
+const shopRouter = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminData.routes);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+const server = http.createServer(app);
+server.listen(3000);
