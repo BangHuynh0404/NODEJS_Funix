38a43846f8b61a96ff8ce434d44f65e98da4f72a
BangHuynh0404 <huynhbangcong@gmail.com>
2022-01-22 00:04:28 +0700

    lab3.5

---
+++ routes/shop.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const adminData = require('./admin');
+router.get('/', (req, res, next) => {
+  res.render('shop', { prods: adminData.products, docTitle: 'shop' });
+});
+
+module.exports = router;
