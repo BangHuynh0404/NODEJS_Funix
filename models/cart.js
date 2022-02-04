f676f15823ee326595d86dc1f23ff67c6fb3f6b9
BangHuynh0404 <huynhbangcong@gmail.com>
2022-02-04 14:46:09 +0700

    lab4.14

--- models/cart.js
+++ models/cart.js
@@ -9,19 +9,19 @@ const p = path.join(
 
 module.exports = class Cart {
   static addProduct(id, productPrice) {
-    //lấy giá trị của giỏ hàng hiện tại
+    // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
       let cart = { products: [], totalPrice: 0 };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
-      // xem trong giỏ có mặt hàng nào, mặt hàng nào đang có sẵn
+      // Analyze the cart => Find existing product
       const existingProductIndex = cart.products.findIndex(
-        (prod) => prod.id === id
+        prod => prod.id === id
       );
       const existingProduct = cart.products[existingProductIndex];
       let updatedProduct;
-      // Thêm mới mặt hàng vào giỏ / chỉ tăng số lượng
+      // Add new product/ increase quantity
       if (existingProduct) {
         updatedProduct = { ...existingProduct };
         updatedProduct.qty = updatedProduct.qty + 1;
@@ -32,7 +32,7 @@ module.exports = class Cart {
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
-      fs.writeFile(p, JSON.stringify(cart), (err) => {
+      fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
     });
