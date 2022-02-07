d363860cb94585ad01c96d32e85d937d7dda275a
BangHuynh0404 <huynhbangcong@gmail.com>
2022-02-07 13:11:19 +0700

    create sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,6 @@
-const mysql = require('mysql2');
-
-const pool = mysql.createPool({
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'node', 'congbang', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'congbang',
 });
-
-module.exports = pool.promise();
+module.exports = sequelize;
