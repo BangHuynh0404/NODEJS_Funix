16a9b9f0fda67d4e8863e8e9296ae1159cc8d499
BangHuynh0404 <huynhbangcong@gmail.com>
2022-02-14 12:39:40 +0700

    add deleteById method to Product in model

--- models/product.js
+++ models/product.js
@@ -46,6 +46,14 @@ class Product {
       })
       .catch((err) => console.log(err));
   }
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => console.log('DELETED!'))
+      .catch((err) => console.log(err));
+  }
 }
 
 module.exports = Product;
