101a58f4a57f3a69df251fea3e358f10b5f2ab45
BangHuynh0404 <huynhbangcong@gmail.com>
2022-02-26 23:10:07 +0700

    lab6.22

--- models/user.js
+++ models/user.js
@@ -61,6 +61,33 @@ class User {
       })
       .catch();
   }
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter((item) => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
+  addOder() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .insertOne(this.cart)
+      .then((result) => {
+        this.cart = { items: [] };
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
+  }
   static findById(userId) {
     const db = getDb();
     return db
