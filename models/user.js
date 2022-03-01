428804fe85c7ba68009bf47296e67defebba62cb
BangHuynh0404 <huynhbangcong@gmail.com>
2022-03-02 00:38:25 +0700

    lab7.3

--- models/user.js
+++ models/user.js
@@ -1,24 +1,3 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const productSchama = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-});
-
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
 
