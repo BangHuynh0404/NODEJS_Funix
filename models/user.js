b11a5002a8a6098f56d66dc078040a622b74f981
BangHuynh0404 <huynhbangcong@gmail.com>
2022-03-03 02:41:28 +0700

    add method removeFromCart

--- models/user.js
+++ models/user.js
@@ -46,6 +46,13 @@ userSchema.methods.addToCart = function (product) {
   this.cart = updatedCart;
   return this.save();
 };
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
@@ -115,10 +122,10 @@ module.exports = mongoose.model('User', userSchema);
 //       });
 //   }
 
-//   deleteItemFromCart(productId) {
-//     const updatedCartItems = this.cart.items.filter(item => {
-//       return item.productId.toString() !== productId.toString();
-//     });
+// deleteItemFromCart(productId) {
+//   const updatedCartItems = this.cart.items.filter(item => {
+//     return item.productId.toString() !== productId.toString();
+//   });
 //     const db = getDb();
 //     return db
 //       .collection('users')
